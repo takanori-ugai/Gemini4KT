0b014827d4d42a6b79115653ccbe2a752e2d43cf
Takanori Ugai <ugai@fujitsu.com>
2024-04-19 12:33:23 +0900

    Refactor BatchEmbedRequest data class to use local import for EmbedContentRequest.

--- src/main/kotlin/io/github/ugaikit/gemini4kt/BatchEmbedRequest.kt
+++ src/main/kotlin/io/github/ugaikit/gemini4kt/BatchEmbedRequest.kt
@@ -2,5 +2,11 @@ package io.github.ugaikit.gemini4kt
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a batch request for embedding content.
+ *
+ * @property requests A list of [EmbedContentRequest] instances,
+ * representing individual embedding requests to be processed in a batch.
+ */
 @Serializable
-data class BatchEmbedRequest(val requests: List<io.github.ugaikit.gemini4kt.EmbedContentRequest>)
+data class BatchEmbedRequest(val requests: List<EmbedContentRequest>)
