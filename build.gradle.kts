9c757b97f054dae8d76a75a0f397a0f8427443bd
Takanori Ugai <ugai@fujitsu.com>
2024-04-20 22:06:17 +0900

    change log library

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ centralPortal {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("io.github.oshai:kotlin-logging-jvm:6.0.3")
-    implementation("ch.qos.logback:logback-classic:1.5.3")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
