7dec11888d86167850d226917106983255dffe77
Takanori Ugai <ugai@fujitsu.com>
2024-04-25 18:22:58 +0900

    Add Tool and ToolConfig data classes for gemini4kt project

--- src/main/kotlin/io/github/ugaikit/gemini4kt/ToolConfig.kt
+++ src/main/kotlin/io/github/ugaikit/gemini4kt/ToolConfig.kt
@@ -3,4 +3,4 @@ package io.github.ugaikit.gemini4kt
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Tools(val functionDeclaration: List<FunctionDeclaration>)
+data class ToolConfig(val functionCallingConfig: FunctionCallingConfig)
